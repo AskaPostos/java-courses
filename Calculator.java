fccb3e044dd1b420626639b2e54ba255220d736c
aska postos <validate.state@gmail.com>
2017-04-22 18:46:01 +0300

    change Calculator

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,9 @@
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("calculate");
+		int arg1 = Integer.valueOf(args[0]);
+		int arg2 = Integer.valueOf(args[1]);
+		int summ = arg1 + arg2;
+		System.out.println("sum:" + summ);
 	}
 }
